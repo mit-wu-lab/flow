6d83ad3c6177551bd509c13728a13832dea6827b
Zhongxia Yan <zxyan@berkeley.edu>
2019-10-31 03:09:15 -0400

    removed warning about needing to restart flow

--- flow/envs/multiagent/base.py
+++ flow/envs/multiagent/base.py
@@ -129,7 +129,7 @@ class MultiEnv(MultiAgentEnv, Env):
         
         return states, reward, done, infos
 
-    def reset(self, new_inflow_rate=None):
+    def reset(self):
         """Reset the environment.
 
         This method is performed in between rollouts. It resets the state of
@@ -147,21 +147,6 @@ class MultiEnv(MultiAgentEnv, Env):
         """
         # reset the time counter
         self.time_counter = 0
-
-        # warn about not using restart_instance when using inflows
-        if len(self.net_params.inflows.get()) > 0 and \
-                not self.sim_params.restart_instance:
-            print(
-                "**********************************************************\n"
-                "**********************************************************\n"
-                "**********************************************************\n"
-                "WARNING: Inflows will cause computational performance to\n"
-                "significantly decrease after large number of rollouts. In \n"
-                "order to avoid this, set SumoParams(restart_instance=True).\n"
-                "**********************************************************\n"
-                "**********************************************************\n"
-                "**********************************************************"
-            )
 
         if self.sim_params.restart_instance or \
                 (self.step_counter > 2e6 and self.simulator != 'aimsun'):
